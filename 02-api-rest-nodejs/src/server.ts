2ee6c11f9b995a29fbf33adea8885ee2b26d0ebc
Wesley Feitosa <jwesleydasilva@gmail.com>
2023-05-03 00:11:37 -0300

    chore: setting eslint

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -2,17 +2,13 @@ import fastify from 'fastify';
 
 const app = fastify();
 
-app.get('/hello', () => {
-  return 'Hello dev';
-});
+app.get('/hello', () => 'Hello dev');
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('HTTP Server Running');
-  });  
+void app
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log('HTTP Server Running');
+	});
 
-
-  
