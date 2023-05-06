4ead5002f7083b37594c5924908755f2ce637c64
Wesley Feitosa <jwesleydasilva@gmail.com>
2023-05-05 22:38:38 -0300

    feat: implementing database with knex

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,8 +1,13 @@
 import fastify from 'fastify';
+import { knex } from './database';
 
 const app = fastify();
 
-app.get('/hello', () => 'Hello dev');
+app.get('/hello', async () => {
+	const tables: unknown[] = await knex('sqlite_schema').select('*');
+
+	return tables;
+});
 
 void app
 	.listen({
