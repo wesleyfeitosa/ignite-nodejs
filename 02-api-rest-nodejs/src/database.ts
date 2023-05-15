f4f4975226d90c06adee89961ecc3ce08238cc43
Wesley Feitosa <jwesleydasilva@gmail.com>
2023-05-14 22:24:56 -0300

    build: preparing api to deploy

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -3,10 +3,10 @@ import { knex as setupKnex, type Knex } from 'knex';
 import { env } from './env';
 
 export const config: Knex.Config = {
-	client: 'sqlite',
-	connection: {
+	client: env.DATABASE_CLIENT,
+	connection: env.DATABASE_CLIENT === 'sqlite' ? {
 		filename: env.DATABASE_URL,
-	},
+	} : env.DATABASE_URL,
 	useNullAsDefault: true,
 	migrations: {
 		extension: 'ts',
