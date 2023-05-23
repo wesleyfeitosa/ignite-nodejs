4cd4dd9154440d9a4fc881d2622c349de53b3ddd
Wesley Feitosa <jwesleydasilva@gmail.com>
2023-05-22 22:41:20 -0300

    feat: prisma and controller

---
+++ 03-api-solid/src/http/controllers/register.ts
@@ -0,0 +1,37 @@
+import { hash } from 'bcryptjs';
+import { type FastifyReply, type FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+import { prisma } from '@/lib/prisma';
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+	const registerBodySchema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string().min(6),
+	});
+
+	const { name, email, password } = registerBodySchema.parse(request.body);
+
+	const userWithSameEmail = await prisma.user.findUnique({
+		where: {
+			email,
+		},
+	});
+
+	if (userWithSameEmail) {
+		return reply.status(409).send();
+	}
+
+	const password_hash = await hash(password, 6);
+
+	await prisma.user.create({
+		data: {
+			name,
+			email,
+			password_hash,
+		},
+	});
+
+	return reply.status(201).send();
+}
---
+++ 03-api-solid/src/http/routes.ts
@@ -0,0 +1,7 @@
+import { type FastifyInstance } from 'fastify';
+
+import { register } from './controllers/register';
+
+export async function appRoutes(app: FastifyInstance) {
+	app.post('/users', register);
+}
