5e0f31e5f5e106841de0c83e78d9327f46229648
Wesley Feitosa <jwesleydasilva@gmail.com>
2023-08-02 11:47:08 -0300

    feat: base entities

---
+++ 04-clean-ddd/src/domain/entities/student.ts
@@ -0,0 +1,14 @@
+import { Entity } from "../../core/entities/entity";
+import { UniqueEntityid } from "../../core/entities/unique-entity-id";
+
+interface StudentProps {
+  name: string;
+}
+
+export class Student extends Entity<StudentProps> {
+  static create(props: StudentProps, id?: UniqueEntityid) {
+    const student = new Student(props, id);
+
+    return student;
+  }
+}
