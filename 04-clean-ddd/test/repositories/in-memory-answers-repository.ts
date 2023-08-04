7eb82c54da1055c8c997ad30c0a5d23a62cb0754
Wesley Feitosa <jwesleydasilva@gmail.com>
2023-08-04 16:14:29 -0300

    feat: fetch question answers

--- 04-clean-ddd/test/repositories/in-memory-answers-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-answers-repository.ts
@@ -1,11 +1,23 @@
 import { type Answer } from '@/domain/forum/enterprise/entities/answer';
 import { type AnswersRepository } from '@/domain/forum/application/repositories/answers-repository';
+import { type PaginationParams } from '../../src/core/repositories/pagination-params';
 
 export class InMemoryAnswersRepository implements AnswersRepository {
 	public items: Answer[] = [];
 
 	async create(answer: Answer) {
 		this.items.push(answer);
+	}
+
+	async bulkCreate(data: Answer[]) {
+		const createdItems: Answer[] = [];
+
+		for (const dataItem of data) {
+			createdItems.push(dataItem);
+			this.items.push(dataItem);
+		}
+
+		return createdItems.length;
 	}
 
 	async save(answer: Answer): Promise<void> {
@@ -22,6 +34,17 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 		}
 
 		return answer;
+	}
+
+	async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+		const ITEMS_BY_PAGE = 20;
+
+		const answers = this.items
+			.filter((item) => item.questionId.toString() === questionId)
+			.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+			.slice((page - 1) * ITEMS_BY_PAGE, page * ITEMS_BY_PAGE);
+
+		return answers;
 	}
 
 	async delete(answer: Answer) {
