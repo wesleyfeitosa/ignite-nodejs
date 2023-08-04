944f73115afbbac3574a67749303d602549a231b
Wesley Feitosa <jwesleydasilva@gmail.com>
2023-08-04 16:31:09 -0300

    feat: comment entity

---
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/comment.ts
@@ -0,0 +1,40 @@
+import { type UniqueEntityid } from '@/core/entities/unique-entity-id';
+import { Entity } from '@/core/entities/entity';
+
+export interface CommentProps {
+	authorId: UniqueEntityid;
+	content: string;
+	createdAt: Date;
+	updatedAt?: Date;
+}
+
+export abstract class Comment<Props extends CommentProps> extends Entity<Props> {
+	private touch() {
+		this.props.updatedAt = new Date();
+	}
+
+	get authorId() {
+		return this.props.authorId;
+	}
+
+	get content() {
+		return this.props.content;
+	}
+
+	set content(content: string) {
+		this.props.content = content;
+		this.touch();
+	}
+
+	get createdAt() {
+		return this.props.createdAt;
+	}
+
+	get updatedAt() {
+		return this.props.updatedAt;
+	}
+
+	get excerpt() {
+		return this.content.substring(0, 120).trimEnd().concat('...');
+	}
+}
